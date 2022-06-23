545662a48c6cfe6db62f901fb848f223557b8fdb
Sergei Chestakov <me@sergei.com>
2022-06-23 21:01:58 +0000

    Fix HMR on Replit

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,5 +1,14 @@
+const isReplit = Boolean(process.env.REPL_SLUG);
+
 export default {
   server: {
     host: '0.0.0.0',
+    ...(isReplit
+      ? {
+          hmr: {
+            clientPort: 443,
+          },
+        }
+      : {}),
   }
 }
